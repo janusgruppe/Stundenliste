5097ca6bd566e783c724f197b0b48fa6b2f1c7a5
Kaya <kaya@janus.at>
2012-05-16 10:20:52 +0200

    XML Writer added

--- Stundenliste/Start.Designer.vb
+++ Stundenliste/Start.Designer.vb
@@ -22,9 +22,71 @@ Partial Class Start
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.InfoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1399, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InformationToolStripMenuItem})
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.InfoToolStripMenuItem.Text = "Hilfe"
+        '
+        'InformationToolStripMenuItem
+        '
+        Me.InformationToolStripMenuItem.Name = "InformationToolStripMenuItem"
+        Me.InformationToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InformationToolStripMenuItem.Text = "Information"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(662, 298)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "btn_test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Start
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1399, 619)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Start"
+        Me.Text = "Stundenliste"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents StartToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InformationToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- Stundenliste/Start.vb
+++ Stundenliste/Start.vb
@@ -1,3 +1,25 @@
-﻿Public Class Start
+﻿Option Explicit On
+Option Strict On
 
+Public Class Start
+    'When Form is started
+    Private Sub Start_Activated(sender As Object, e As System.EventArgs) Handles Me.Activated
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub InformationToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles InformationToolStripMenuItem.Click
+        Info.Show()
+    End Sub
+    '******************************
+    'XML
+    '******************************
+    Private Sub XMLWriter(ByVal txt_bereich As String, ByVal txt_info As String, ByVal date_time As Date)
+        Dim myWriter As New IO.StreamWriter("log.xml", True)
+        myWriter.WriteLine("<Log " & txt_bereich & "=" & Chr(34) & txt_info & Chr(34) & " Datum=" & Chr(34) & date_time.ToString("G") & Chr(34) & " " & Chr(47) & ">")
+        myWriter.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        XMLWriter("test", "Das ist ein Test", Date.Now)
+    End Sub
 End Class
